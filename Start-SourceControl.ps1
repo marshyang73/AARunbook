3497e5c92b770fcb7428f2f90216af9406a3a51c
marshyang73 <48205493+marshyang73@users.noreply.github.com>
2019-03-06 16:17:59 +0800

    Update Start-SourceControl.ps1

--- Start-SourceControl.ps1
+++ Start-SourceControl.ps1
@@ -4,4 +4,9 @@ workflow Start-SourceControl
     #This is my first Source Control Runbook
 
     Write-Output "Azure Automation"
+    
+    Write-Output "Test again 2"
+    
+    Write-Output "Test again 3"
+    
 }
